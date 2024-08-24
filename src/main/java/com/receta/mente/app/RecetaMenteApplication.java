e4b6faffe8c1b9175c7f17e2db6d96f2c2fb5636
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-24 18:18:37 -0400

    initial commit

---
+++ src/main/java/com/receta/mente/app/RecetaMenteApplication.java
@@ -0,0 +1,13 @@
+package com.receta.mente.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetaMenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetaMenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/receta/mente/app/RecetaMenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receta.mente.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetaMenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
