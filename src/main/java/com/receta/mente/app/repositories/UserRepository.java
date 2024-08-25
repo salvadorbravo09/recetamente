cec7e4ce374919d35cd9abcfe206ece829d8ade2
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-24 22:14:55 -0400

    se creo la interfaz para la gestión de usuarios en la base de datos

---
+++ src/main/java/com/receta/mente/app/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.receta.mente.app.repositories;
+
+import com.receta.mente.app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmail(String email);
+}
