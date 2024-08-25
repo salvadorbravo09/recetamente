0d55f195f235fb11621392173541ed13d5fd4f78
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-24 22:11:47 -0400

    se creo la entidad para el usuario

---
+++ src/main/java/com/receta/mente/app/models/User.java
@@ -0,0 +1,22 @@
+package com.receta.mente.app.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+}
